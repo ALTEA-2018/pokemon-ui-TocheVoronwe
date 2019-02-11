0884df3a8ed3cb684ff4fdc7f06115d11ca0487a
toche <christophe.maingard@epitech.eu>
2019-02-11 10:38:37 +0100

    feat(pokemon): register and index

--- src/main/java/com/miage/altea/tp/pokemon_ui/controller/IndexController.java
+++ src/main/java/com/miage/altea/tp/pokemon_ui/controller/IndexController.java
@@ -2,6 +2,8 @@ package com.miage.altea.tp.pokemon_ui.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller("index")
 public class IndexController {
@@ -9,6 +11,14 @@ public class IndexController {
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+    @PostMapping("/registerTrainer")
+    ModelAndView registerNewTrainer(String trainerName){
+        var modelAndView = new ModelAndView("register");
+
+        modelAndView.addObject("name", trainerName);
+        return modelAndView;
     }
 
 }
