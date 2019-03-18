925584b4f747cfd3d3de07639de28607fab0773d
toche <christophe.maingard@epitech.eu>
2019-03-18 11:48:39 +0100

    feat(ui): add profile page

--- src/main/java/com/miage/altea/tp/pokemon_ui/controller/TrainerController.java
+++ src/main/java/com/miage/altea/tp/pokemon_ui/controller/TrainerController.java
@@ -2,6 +2,8 @@ package com.miage.altea.tp.pokemon_ui.controller;
 
 import com.miage.altea.tp.pokemon_ui.trainers.service.TrainerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,6 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class TrainerController {
     TrainerService trainerService;
+
     @GetMapping("/trainer")
     public ModelAndView trainer(){
         var modelAndView = new ModelAndView("trainer");
@@ -16,9 +19,24 @@ public class TrainerController {
         return modelAndView;
     }
 
+    @GetMapping("/profile")
+    public ModelAndView profile() {
+        var connectedUser = getConnectedUsername();
+        var modelAndView = new ModelAndView("profile");
+        modelAndView.addObject("trainer", trainerService.getTrainer(connectedUser));
+        return modelAndView;
+
+    }
+
     @Autowired()
     void setTrainerService(TrainerService trainerService) {
         this.trainerService = trainerService;
+    }
+
+    private String getConnectedUsername() {
+        var context = SecurityContextHolder.getContext();
+        User principal = (User)context.getAuthentication().getPrincipal();
+        return principal.getUsername();
     }
 
 }
