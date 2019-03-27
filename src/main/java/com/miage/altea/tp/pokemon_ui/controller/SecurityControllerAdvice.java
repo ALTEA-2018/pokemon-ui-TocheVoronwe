f928f60f9d5e8595ef477aba99dab781578168f4
toche <christophe.maingard@epitech.eu>
2019-03-27 20:29:50 +0100

    feat(securityController): add securityController class and tests

--- src/main/java/com/miage/altea/tp/pokemon_ui/controller/SecurityControllerAdvice.java
+++ src/main/java/com/miage/altea/tp/pokemon_ui/controller/SecurityControllerAdvice.java
@@ -1,17 +1,13 @@
 package com.miage.altea.tp.pokemon_ui.controller;
 
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
-import java.security.Principal;
-
-@Controller
+@ControllerAdvice
 public class SecurityControllerAdvice {
 
-    
+    @ModelAttribute(value = "user")
     public Object principal(){
         var context = SecurityContextHolder.getContext();
         return context.getAuthentication().getPrincipal();
