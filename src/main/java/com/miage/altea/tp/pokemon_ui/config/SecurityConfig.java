5c6bf75ab33aca4314077c80ef0e3d86f69e8d3b
toche <christophe.maingard@epitech.eu>
2019-04-05 18:43:13 +0200

    feat(pokemon): add pom.xml

--- src/main/java/com/miage/altea/tp/pokemon_ui/config/SecurityConfig.java
+++ src/main/java/com/miage/altea/tp/pokemon_ui/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -35,5 +36,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .map(trainer ->
                         User.withUsername(trainer.getName()).password(trainer.getPassword()).roles("USER").build())
                 .orElseThrow(() -> new BadCredentialsException("No such user"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.csrf().ignoringAntMatchers("/api/**");
     }
 }
